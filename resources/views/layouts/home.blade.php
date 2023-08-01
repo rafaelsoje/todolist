6f2465b51c7bdb1ab2221db63811ab2993819485
Rafael Soje <rafaelsoje@gmail.com>
2023-08-01 17:05:13 -0300

    create layout

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./assets/css/style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&display=swap" rel="stylesheet">
+    <title>Todo List</title>
+</head>
+<body>
+<div class="container">
+    <div class="sidebar">
+        logo
+    </div>
+    <div class="content">
+        <nav>
+            <a href="#" class="btn btn-primary">
+                Criar Tarefa
+            </a>
+        </nav>
+        <main>
+            <section class="graph">
+                <div class="graph_header">
+                    <h2>Progresso do dia.</h2>
+                    <hr/>
+                    Data
+                </div>
+            </section>
+            <section class="list">
+                Lista
+            </section>
+        </main>
+    </div>
+</div>
+
+</body>
+</html>
